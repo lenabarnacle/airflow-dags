e3b62279cf33a21f0f96c88ada7d5e306e6ebd56
Brovko Nikita <brovko.ns@gmail.com>
2022-12-13 18:03:13 +0600

    Отключил проверку

--- dags/sales_financial_department.py
+++ dags/sales_financial_department.py
@@ -244,25 +244,25 @@ def __main__():
 
             df = pd.read_excel(FILE_PATH, sheet_name="All sales data")
             columns_name_cheсk = validate_columns(df, COL_MAPPER)
-            sales_month_check = validate_sales_month(df)
+            # sales_month_check = validate_sales_month(df)
             try:
                 email_data = pd.read_excel(FILE_PATH, sheet_name="info")
                 email = email_data["email"][0]
             except:
                 email = None
-            if not sales_month_check:
-                if email:
-                    send_email(
-                        to=[email],
-                        subject=f"Отчет о загрузки файла {file_name}",
-                        html_content="В столбце 'Месяц' не все значения содержат x перед 'Датой'",
-                        cc=None,
-                        bcc=None,
-                    )
-                    logging.warning(
-                        f"В файле {file_name} в столбце 'Месяц' не все значения содержат x перед 'Датой'"
-                    )
-                    client.delete_object(Bucket=BUCKET, Key=file_name)
+                # if not sales_month_check:
+                #     if email:
+                #         send_email(
+                #             to=[email],
+                #             subject=f"Отчет о загрузки файла {file_name}",
+                #             html_content="В столбце 'Месяц' не все значения содержат x перед 'Датой'",
+                #             cc=None,
+                #             bcc=None,
+                #         )
+                #         logging.warning(
+                #             f"В файле {file_name} в столбце 'Месяц' не все значения содержат x перед 'Датой'"
+                #         )
+                #         client.delete_object(Bucket=BUCKET, Key=file_name)
 
                 return
             if not columns_name_cheсk:
